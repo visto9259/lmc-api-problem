1cee48a5f177405ed7234b62dd379c79eabcf0fb
Eric Richer eric.richer@vistoconsulting.com <eric.richer@vistoconsulting.com>
2026-01-27 10:20:07 -0500

    Initial version

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lmc\Api\Problem\Exception;
+
+final class InvalidArgumentException extends \InvalidArgumentException implements ExceptionInterface
+{
+}
